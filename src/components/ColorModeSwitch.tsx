23f8c2299ef6dddeadea908dee5f6e2b1bcae4b7
Mouli <128670409+Moulithar@users.noreply.github.com>
2024-03-26 15:55:11 +0530

    implemented color mode

---
+++ src/components/ColorModeSwitch.tsx
@@ -0,0 +1,14 @@
+import { Switch, useColorMode } from "@chakra-ui/react";
+
+const ColorModeSwitch = () => {
+  const { toggleColorMode, colorMode } = useColorMode();
+  return (
+    <Switch
+      colorScheme={"green"}
+      isChecked={colorMode === "dark"}
+      onChange={toggleColorMode}
+    />
+  );
+};
+
+export default ColorModeSwitch;
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,11 +1,13 @@
 import { HStack, Image } from "@chakra-ui/react";
 
 import logo from "../assets/react.svg";
+import ColorModeSwitch from "./ColorModeSwitch";
 
 const NavBar = () => {
   return (
-    <HStack>
-      <Image src={logo} boxSize="40px"/>
+    <HStack justifyContent={"space-between"} padding={"0px 20px"}>
+      <Image src={logo} boxSize="40px" />
+      <ColorModeSwitch />
     </HStack>
   );
 };
