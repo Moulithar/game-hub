6fdd2dcf9ed538aa0a1b1ca7b9b3817bc4112eab
Mouli <128670409+Moulithar@users.noreply.github.com>
2024-03-29 09:09:58 +0530

    emoji added

---
+++ src/components/Emoji.tsx
@@ -0,0 +1,22 @@
+import { Icon, IconProps } from "@chakra-ui/react";
+import { BsFillHandThumbsUpFill } from "react-icons/bs";
+import { FiMeh } from "react-icons/fi";
+import { GiBullseye } from "react-icons/gi";
+
+interface Props {
+  rating: number;
+}
+
+const Emoji = ({ rating }: Props) => {
+  const emojiMap: { [key: number]: React.ElementType } = {
+    3: FiMeh,
+    4: BsFillHandThumbsUpFill,
+    5: GiBullseye,
+  };
+
+  const color = rating === 5 ? "#711414" : "#ffd42a";
+  const boxSize = rating === 5 ? 30 : 25;
+  return <Icon as={emojiMap[rating]} boxSize={boxSize} color={color} />;
+};
+
+export default Emoji;
--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,8 +1,9 @@
-import { Card, CardBody, HStack, Heading, Image } from "@chakra-ui/react";
+import { Box, Card, CardBody, HStack, Heading, Image } from "@chakra-ui/react";
 import { Game } from "../hooks/useGames";
 import getCroppedImages from "../services/image-url";
 import CrtiticScore from "./CrtiticScore";
 import PlatformIconList from "./PlatformIconList";
+import Emoji from "./Emoji";
 
 interface Props {
   game: Game;
@@ -10,16 +11,21 @@ interface Props {
 
 const GameCard = ({ game }: Props) => {
   return (
-    <Card >
+    <Card>
       <Image src={getCroppedImages(game.background_image)} />
       <CardBody>
-        <HStack justifyContent={"space-between"} marginBottom={3}> 
+        <HStack justifyContent={"space-between"} marginBottom={3}>
           <PlatformIconList
             platforms={game.parent_platforms.map((p) => p.platform)}
           />
           <CrtiticScore score={game.metacritic} />
         </HStack>
-        <Heading fontSize={"2xl"}>{game.name}</Heading>
+        <Heading fontSize={"2xl"}>
+          {game.name}
+          <Box marginTop={2}>
+            <Emoji rating={game.rating_top} />
+          </Box>{" "}
+        </Heading>
       </CardBody>
     </Card>
   );
